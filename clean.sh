cf1694626cb5a1812fa0b31a855dcb9897e72915
xxyy <devnull@nowak-at.net>
2014-03-01 17:45:02 +0100

    Update clean and init scripts to be more intuitive

--- clean.sh
+++ clean.sh
@@ -1,6 +1,30 @@
-echo "Cleaning..."
-if [ -d CloudBot/plugins/data ]; then
-    cp -r CloudBot/plugins/data data
+echo "This is going to remove your current CloudBot working copy! (But backup the plugins/data/ folder)"
+# Taken from http://stackoverflow.com/a/1885534/1117552
+read -p "Are you sure that you want to update with CloudBot to your SpagtBabe patches? " -n 1 -r
+echo    # move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    echo "Your data is safe!"
+    exit 1
 fi
+#end
+
+echo "Cleaning..."
+
+if [ -d data/ ]; then
+    mv -f data/ data_backup
+    echo "Made backup of old data backup to data_backup/!"
+fi
+
+if [ -d CloudBot/plugins/data/ ]; then
+    cp -R CloudBot/plugins/data/ data
+    echo "Made backup of CloudBot/plugins/data/ to data/!"
+else
+    echo "Ummm... You do have plugin data, right? Did not find it."
+fi
+
+echo "Now removing CloudBot working copy..."
 rm -rf CloudBot
+echo "Removed old CloudBot working copy!"
+
 echo "Done cleaning!"
--- init.sh
+++ init.sh
@@ -1,35 +1,51 @@
 #!/bin/bash
 echo "=======================Cleaning"
+
 ./clean.sh
+if [ "$?" != "0" ]; then
+    echo "Cleaning failed!"
+fi
+
 echo "========================Cloning"
-git submodule add https://github.com/ClouDev/CloudBot.git
-cd CloudBot
+
+git submodule add -f https://github.com/ClouDev/CloudBot.git
+if [ -d CloudBot/ ]; then
+    cd CloudBot
+else
+    echo "Could not cd to CloudBot/ directory! This means that cloning failed in most cases. Exiting!"
+    exit 1
+fi
+
 echo "=======================Patching"
+
 echo "Applying patches!"
 git am -3 ../patches/*.patch
+
 if [ "$?" != "0" ]; then
     echo "========================Failing" #I R MAVEN!!1
     echo "PATCH FAILED"
     echo "==============================="
+    
+    cd ../
 else
     echo "=====================Succeeding" #I R MAVEN!!1
     echo "PATCH SUCCESS"
     echo "==============================="
     
-    if [ -f ../config ]; then
+    cd ../
+    
+    if [ -f config ]; then
         echo "Found config! Copying."
-        cp ../config config
+        cp config CloudBot/config
     else
         echo "Config not found! Copying default config (please edit CloudBot/config)"
-        echo "If you want your config there automagically, make a file with name config in the main directory."
-        cp config.default config
+        echo "If you want your config there automagically, make a file named `config` in the SpagtBabe root directory."
+        cp CloudBot/config.default CloudBot/config
     fi
     
-    if [ -d ../data ]; then
+    if [ -d data ]; then
         echo "Found previous data! Copying!"
-        cp -r ../data plugins/data
+        cp -r data CloudBot/plugins/data
     fi
 fi
-
-cd ../
-echo "Bye!"
+echo "KTHXBYE"
